ed9b9e3a40d1fcef5cfd012af20c6004f3c2c881
carlos luis gutierrez ramos <carlos5t@hotmail.com>
2019-06-04 17:12:57 -0500

    Modulo E-Ticket Success

--- PasarelaWeb/application/views/v_e-ticket.php
+++ PasarelaWeb/application/views/v_e-ticket.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-8">
-
+    
             <div class="accordion destinos--2" id="accordionStar">
                 <div class="card">
                     <div class="card-header" id="headingOne">
@@ -45,7 +45,7 @@
                         </a>
                     </div>
                     <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionStar">
-                        <div class="card-body">
+                        <div class="card-body table-responsive">
                             <div class="row">
                                 <div class="col-11">
                                     <table class="table resumen">
@@ -54,8 +54,8 @@
                                                 <th>&nbsp;</th>
                                                 <th>Apellidos / Nombres</th>
                                                 <th>Documento</th>
-                                                <th>Código de Reserva</th>
-                                                <th>&nbsp;</th><!--                                                            
+                                                <th>N° Ticket</th>
+                                                <th>Boleto</th><!--                                                            
                                                 <th>Boleto</th>-->
                                             </tr>
                                         </thead>
@@ -63,17 +63,19 @@
 
                                             <?php
                                             $pax = 1;
-                                            foreach ($res_model_pasajero->Result() as $item) {
-                                                ?>
+                                            $indice = 0;
+                                             foreach ($Pasajeros as $Pasajero) { ?>
+                                                
                                                 <tr>
                                                     <td>Pasajero <?= $pax ?></td>
-                                                    <td><?php echo $item->nombres . " " . $item->apellidos; ?></td>
-                                                    <td><?php echo $item->tipo_documento . " " . $item->num_documento; ?></td>                                                                      
-                                                    <td><?php echo $item->num_ticket; ?></td>     
-                                                    <td><button type="button" class="btn btn-outline-danger btn_ver_ticket" id="<?= $item->num_ticket ?>">Ver Ticket</button></td>
+                                                    <td><?= $Pasajero->Customer->PersonName->Surname ?>/<?= $Pasajero->Customer->PersonName->GivenName ?></td>
+                                                    <td><?= $Pasajero->Customer->Document->attributes()->DocType ?> : <?= $Pasajero->Customer->Document->attributes()->DocID ?></td>
+                                                    <td><?= $TravelItinerary->ItineraryInfo->Ticketing[$indice]->attributes()->eTicketNumber ?></td>
+                                                    <td><button type="button" class="btn btn-outline-danger btn_ver_ticket" id="<?= $TravelItinerary->ItineraryInfo->Ticketing[$indice]->attributes()->eTicketNumber ?>">Ver Ticket</button></td>
                                                 </tr>
                                                 <?php
                                                 $pax++;
+                                                $indice++;
                                             }
                                             ?>
 
@@ -91,60 +93,35 @@
                         </a>
                     </div>
                     <div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent="#accordionStar" style="">
-                        <div class="card-body">
-                            <div class="row">
+                        <div class="card-body table-responsive">
+                        <div class="row">
                                 <div class="col-11">
                                     <table class="table resumen">
+                                        <thead>
+                                            <tr>
+                                                <th>Día</th>
+                                                <th>Salida / Llegada</th>
+                                                <th>Origen</th>
+                                                <th>Destino</th>
+                                                <th>Vuelo</th>
+                                            </tr>
+                                        </thead>
                                         <tbody>
-                                            <tr>
-                                                <th>Código de reserva:</th>
-                                                <td><?php echo $res_model_vuelo->pnr ?></td>
-                                            </tr>
-                                            <tr>
-                                                <th>Origen :</th>
-                                                <td><?php echo $res_model_vuelo->origen ?></td>
-                                            </tr>
-                                            <tr>
-                                                <th>Destino :</th>
-                                                <td> <?php echo $res_model_vuelo->destino ?></td>
-                                            </tr>
 
-                                            <tr>
-                                                <th>Hora Salida:</th>
-                                                <td>
-                                                    <?php
-                                                    $hora_salida_Salida = new DateTime($res_model_vuelo->fechahora_salida_tramo_ida);
-                                                    echo $hora_salida_Salida->format('H:i');
-                                                    ?>
-                                                </td>
-
-                                            </tr>
-
-                                            <tr>
-                                                <th>Hora Llegada :</th>
-                                                <td>
-                                                    <?php
-                                                    $hora_salida_ida = new DateTime($res_model_vuelo->fechahora_llegada_tramo_ida);
-                                                    echo $hora_salida_ida->format('H:i');
-                                                    ?>
-                                                </td>
-                                            </tr>
-
-                                            <tr>
-                                                <th>Vuelo Ida :</th>
-                                                <td> <?php echo $res_model_vuelo->num_vuelo_ida ?></td>
-                                            </tr>
-                                            <?php if ($res_model_vuelo->num_vuelo_retorno != 'NULL') { ?>
+                                            <?php foreach ($Itinerarios as $Itinerario) { ?>
                                                 <tr>
-                                                    <th>Vuelo Retorno :</th>
-                                                    <td> <?php echo $res_model_vuelo->num_vuelo_retorno ?></td>
+                                                    <td><?= (new DateTime(($Itinerario->Air->Reservation->attributes()->DepartureDateTime)))->format('d/M') ?></td>
+                                                    <td><?= (new DateTime(($Itinerario->Air->Reservation->attributes()->DepartureDateTime)))->format('h:i') ?>/<?= (new DateTime(($Itinerario->Air->Reservation->attributes()->ArrivalDateTime)))->format('h:i') ?></td>
+                                                    <td><?= $Itinerario->Air->Reservation->DepartureAirport->attributes()->LocationCode ?></td>
+                                                    <td><?= $Itinerario->Air->Reservation->ArrivalAirport->attributes()->LocationCode ?></td>
+                                                    <td><?= $Itinerario->Air->Reservation->attributes()->FlightNumber ?> <?= (strlen($Itinerario->Air->Reservation->attributes()->FlightNumber) == 4) ? ' | Operado por Peruavian' : '' ?></td>
                                                 </tr>
-
-                                                <?php
-                                            }
+                                            <?php }
                                             ?>
+                                
                                         </tbody>
                                     </table>
+
                                 </div>
                             </div>
 
@@ -158,7 +135,6 @@
                                             </tr>
                                         </tbody>
                                     </table>
-                                    </di</div>
                             </div>
                         </div>
                     </div>
